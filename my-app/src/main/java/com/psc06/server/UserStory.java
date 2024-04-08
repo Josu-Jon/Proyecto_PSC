19d30b190ba745255731db898865be8e048cc0cb
Ibai Carracedo <47413687+ibaicarracedo@users.noreply.github.com>
2024-04-08 17:25:21 +0200

    Added userstory class

---
+++ my-app/src/main/java/com/psc06/server/UserStory.java
@@ -0,0 +1,39 @@
+package com.psc06.server;
+
+public class UserStory {
+    
+    int id;
+    String userStory;
+    int estimation;
+    int pbPriority;
+
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserStory() {
+        return userStory;
+    }
+    public void setUserStory(String userStory) {
+        this.userStory = userStory;
+    }
+
+    public int getEstimation() {
+        return estimation;
+    }
+    public void setEstimation(int estimation) {
+        this.estimation = estimation;
+    }
+    
+    public int getPbPriority() {
+        return pbPriority;
+    }
+    public void setPbPriority(int pbPriority) {
+        this.pbPriority = pbPriority;
+    }
+
+}
