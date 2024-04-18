44cc5ef9c9eebc263161dd63411c1837649dca2d
Ibai Carracedo <47413687+ibaicarracedo@users.noreply.github.com>
2024-04-18 15:52:10 +0200

    Added new functions on server side. DeleteSprint(), deleteUserStory(), and reassignUserStory(). Updated registerUserStory to update parameters if the story already exists.

--- src/main/java/com/psc06/client/ExampleClient.java
+++ src/main/java/com/psc06/client/ExampleClient.java
@@ -1,5 +1,6 @@
 package com.psc06.client;
 
+import javax.jdo.Query;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -12,6 +13,7 @@ import javax.ws.rs.core.Response.Status;
 import com.psc06.pojo.SprintData;
 import com.psc06.pojo.SprintStoryData;
 import com.psc06.pojo.UserStoryData;
+import com.psc06.server.jdo.UserStory;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -80,7 +82,7 @@ public class ExampleClient {
 		Invocation.Builder invocationBuilder = registerUserWebTarget.request(MediaType.APPLICATION_JSON);
 		
 		SprintData sp = new SprintData();
-		sp.setSprintNum(id);
+		sp.setSprintNum(sprintId);
 		UserStoryData newStory = new UserStoryData();
 		newStory.setId(id);
 		newStory.setUserStory(userStory);
@@ -99,6 +101,59 @@ public class ExampleClient {
 	}
 
 
+	public void deleteSprint(int sprintId) {
+		WebTarget registerUserWebTarget = webTarget.path("deleteSprint");
+		Invocation.Builder invocationBuilder = registerUserWebTarget.request(MediaType.APPLICATION_JSON);
+
+		SprintData sp = new SprintData();
+		sp.setSprintNum(sprintId);
+
+		Response response = invocationBuilder.post(Entity.entity(sp, MediaType.APPLICATION_JSON));
+		if (response.getStatus() != Status.OK.getStatusCode()) {
+			logger.error("Error connecting with the server. Code: {}", response.getStatus());
+		} else {
+			logger.info("Sprint " + sp.getSprintNum() + " deleted.");
+		}
+	}
+
+	public void deleteUserStory(int id) {
+		WebTarget registerUserWebTarget = webTarget.path("deleteUserStory");
+		Invocation.Builder invocationBuilder = registerUserWebTarget.request(MediaType.APPLICATION_JSON);
+
+		UserStoryData story = new UserStoryData();
+		story.setId(id);
+
+		Response response = invocationBuilder.post(Entity.entity(story, MediaType.APPLICATION_JSON));
+		if (response.getStatus() != Status.OK.getStatusCode()) {
+			logger.error("Error connecting with the server. Code: {}", response.getStatus());
+		} else {
+			logger.info("User Story " + story.getId() + " deleted.");
+		}
+	}
+
+	public void reassignUserStory(int sprintId, int id, String userStory, int estimation, int pbPriority) {
+		WebTarget registerUserWebTarget = webTarget.path("reassignUserStory");
+		Invocation.Builder invocationBuilder = registerUserWebTarget.request(MediaType.APPLICATION_JSON);
+		
+		SprintData sp = new SprintData();
+		sp.setSprintNum(sprintId);
+		UserStoryData newStory = new UserStoryData();
+		newStory.setId(id);
+		newStory.setUserStory(userStory);
+		newStory.setEstimation(estimation);
+		newStory.setPbPriority(pbPriority);
+		SprintStoryData sprintStory = new SprintStoryData();
+		sprintStory.setSprintData(sp);
+		sprintStory.setUserStoryData(newStory);
+
+		Response response = invocationBuilder.post(Entity.entity(sprintStory, MediaType.APPLICATION_JSON));
+		if (response.getStatus() != Status.OK.getStatusCode()) {
+			logger.error("Error connecting with the server. Code: {}", response.getStatus());
+		} else {
+			logger.info("User Story correctly deleted from sprint. ");
+		}
+	}
+
 
 	public static void main(String[] args) {
 		if (args.length != 2) {
@@ -114,8 +169,7 @@ public class ExampleClient {
 		conSer.registerUserStory(id1, userstory1, est1, pb1);
 		conSer.registerUserStory(id2, userstory2, est2, pb2);
 		conSer.assignUserStory(sprintId, id1, userstory1, est1, pb1);
-
-
+		conSer.reassignUserStory(sprintId, id1, userstory1, est1, pb1);
 		
 	}
 }
--- src/main/java/com/psc06/server/Resource.java
+++ src/main/java/com/psc06/server/Resource.java
@@ -53,12 +53,7 @@ public class Resource {
 			} catch (javax.jdo.JDOObjectNotFoundException jonfe) {
 				logger.info("Exception launched: {}", jonfe.getMessage());
 			}
-
-			if (sprint != null) {
-				logger.info("Sprint: {}", sprint);
-			}
 			
-
 			if (sprint != null) {
 				logger.info("Sprint already created: {}", sprint);
 			} else {
@@ -66,6 +61,39 @@ public class Resource {
 				sprint = new Sprint(sprintData.getSprintNum());
 				pm.makePersistent(sprint);					 
 				logger.info("Sprint created: {}", sprint);
+			}
+			tx.commit();
+			return Response.ok().build();
+        }
+        finally
+        {
+            if (tx.isActive())
+            {
+                tx.rollback();
+            }
+      
+		}
+	}
+
+	@POST
+	@Path("/deleteSprint")
+	public Response deleteSprint(SprintData sprintData) {
+		try
+        {	
+            tx.begin();
+            logger.info("Checking whether the user already exits or not: '{}'", sprintData.getSprintNum());
+			Sprint sprint = null;
+			try {
+				sprint = pm.getObjectById(Sprint.class, sprintData.getSprintNum());
+			} catch (javax.jdo.JDOObjectNotFoundException jonfe) {
+				logger.info("Exception launched: {}", jonfe.getMessage());
+			}
+
+			if (sprint != null) {
+				logger.info("Deleting Sprint: {}", sprint);
+				pm.deletePersistent(sprint);
+			} else {
+				logger.info("Sprint doesn't exist.");
 			}
 			tx.commit();
 			return Response.ok().build();
@@ -99,12 +127,49 @@ public class Resource {
 			}
 
 			if (story != null) {
-				logger.info("Story already created: {}", story);
+				logger.info("Story already created, updating values: {}", story);
+				story.setUserStory(userStoryData.getUserStory());
+				story.setPbPriority(userStoryData.getPbPriority());
+				story.setEstimation(userStoryData.getEstimation());
+				logger.info("Updated values: {}", story);
 			} else {
 				logger.info("Creating story: {}", story);
 				story = new UserStory(userStoryData.getId(), userStoryData.getUserStory(), userStoryData.getEstimation(), userStoryData.getPbPriority());
 				pm.makePersistent(story);					 
 				logger.info("User Story created: {}", story);
+			}
+			tx.commit();
+			return Response.ok().build();
+        }
+        finally
+        {
+            if (tx.isActive())
+            {
+                tx.rollback();
+            }
+      
+		}
+	}
+
+	@POST
+	@Path("/deleteUserStory")
+	public Response deleteUserStory(UserStoryData userStoryData) {
+		try
+        {	
+            tx.begin();
+            logger.info("Checking whether the user story already exits or not: '{}'", userStoryData.getId());
+			UserStory story = null;
+			try {
+				story = pm.getObjectById(UserStory.class, userStoryData.getId());
+			} catch (javax.jdo.JDOObjectNotFoundException jonfe) {
+				logger.info("Exception launched: {}", jonfe.getMessage());
+			}
+
+			if (story != null) {
+				logger.info("Deleting User Story: {}", story.toString());
+				pm.deletePersistent(story);
+			} else {
+				logger.info("User Story doesn't exist.");
 			}
 			tx.commit();
 			return Response.ok().build();
@@ -178,5 +243,103 @@ public class Resource {
 		}
 	}
 
+	@POST
+	@Path("/reassignUserStory")
+	public Response reassignUserStory(SprintStoryData sprintStoryData) {
+		Sprint sprint = null;
+		UserStory story = null;
+		try {
+			tx.begin();
+			logger.info("Creating query ...");
+
+			try (Query<?> q = pm.newQuery(Sprint.class)) {
+				q.setFilter("this.num == :num");
+				q.setUnique(true);
+				sprint = (Sprint) q.execute(sprintStoryData.getSprintData().getSprintNum());
+
+				logger.info("Sprint retrieved: {}", sprint.toString());
+				if (sprint != null) {
+
+					
+					try {
+						story = pm.getObjectById(UserStory.class, sprintStoryData.getUserStoryData().getId());
+					} catch (javax.jdo.JDOObjectNotFoundException jonfe) {
+						logger.info("Exception launched: {}", jonfe.getMessage());
+					}
+
+					if (story != null) {
+						sprint.getAllStories().remove(story);
+						pm.makePersistent(sprint);
+						logger.info("Deleted story {} from sprint {}! ", story, sprint);
+					} else {
+						logger.info("Error. El user story introducido no existe. ");
+					}
+					return Response.ok().build();
+
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			tx.commit();
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+		}
+		return Response.status(Status.BAD_REQUEST).entity("Los datos implementados no son correctos").build();
+
+	}
+
+
+	@SuppressWarnings("unchecked")
+	@GET
+	@Path("/getAllUserStories")
+	public Query<UserStory> getAllUserStories() {
+
+		Query<UserStory> queryUserStory = null;
+
+		try {
+			tx.begin();
+			logger.info("Creating query ...");
+
+			try (Query<?> q = pm.newQuery(UserStory.class)) {
+				
+				if (q != null) {
+					queryUserStory = (Query<UserStory>) q.execute();
+					logger.info("User Stories retrieved.");
+				}
+				
+				if (queryUserStory != null) {
+					return queryUserStory;
+				}
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			tx.commit();
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+		}
+
+		return null;
+	}
+
+	/*
+	@GET
+	@Path("/getUserStory")
+	public Response getUserStory(SprintData sprint) {
+		
+	}
+
+	@POST
+	@Path("/setUserStoryParams")
+	public Response setUserStoryParams(UserStoryData userStoryData) {
+		
+	}
+	*/
 }
 
