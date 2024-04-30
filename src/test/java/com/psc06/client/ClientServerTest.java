a7145b19501a9d5115bd84789ee6236e91009964
ThePikaTamer <thepikatamer@gmail.com>
2024-04-30 16:21:31 +0200

    Creada clase test

---
+++ src/test/java/com/psc06/client/ClientServerTest.java
@@ -0,0 +1,15 @@
+package com.psc06.client;
+
+import org.junit.Test;
+
+public class ClientServerTest extends ClientServer
+{
+    public ClientServerTest(String hostname, String port) 
+    {
+        super(hostname, port);
+        //TODO Auto-generated constructor stub
+    }
+
+    ClientServer cs;
+
+}
