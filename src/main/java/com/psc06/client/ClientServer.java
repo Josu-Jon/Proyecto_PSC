a902423c1224114b00aa930ef6971ff8fa761171
Ibai Carracedo <47413687+ibaicarracedo@users.noreply.github.com>
2024-05-01 18:21:11 +0200

    Fixed merged errors.

--- src/main/java/com/psc06/client/ClientServer.java
+++ src/main/java/com/psc06/client/ClientServer.java
@@ -1,51 +1,5 @@
 package com.psc06.client;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.Invocation;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
-
-import com.psc06.pojo.UserStoryData;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-public class ClientServer {
-
-	protected static final Logger logger = LogManager.getLogger();
-
-	private Client client;
-	private WebTarget webTarget;
-
-	public ClientServer(String hostname, String port) {
-		client = ClientBuilder.newClient();
-		webTarget = client.target(String.format("http://%s:%s/rest/resource", hostname, port));
-	}
-
-	public void registerUserStory(int id, String userStory, int estimation, int pbPriority) {
-		WebTarget registerUserWebTarget = webTarget.path("registerUserStory");
-		Invocation.Builder invocationBuilder = registerUserWebTarget.request(MediaType.APPLICATION_JSON);
-		
-		UserStoryData newStory = new UserStoryData();
-		newStory.setId(id);
-		newStory.setUserStory(userStory);
-		newStory.setEstimation(estimation);
-		newStory.setPbPriority(pbPriority);
-		Response response = invocationBuilder.post(Entity.entity(newStory, MediaType.APPLICATION_JSON));
-		if (response.getStatus() != Status.OK.getStatusCode()) {
-			logger.error("Error connecting with the server. Code: {}", response.getStatus());
-		} else {
-			logger.info("User Story correctly created");
-		}
-	}
-}
-
-package com.psc06.client;
-
 import java.util.*;
 import java.lang.reflect.Type;
 import java.util.Collection;
