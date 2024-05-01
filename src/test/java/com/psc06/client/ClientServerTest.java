ab50bf206c308f3f7897441a6e698a352e93492e
Josu Jon Lopez Arribas <josujon.l.a@opendeusto.es>
2024-05-01 21:39:35 +0200

    ClientServer Test Vacios

---
+++ src/test/java/com/psc06/client/ClientServerTest.java
@@ -0,0 +1,90 @@
+package com.psc06.client;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import java.util.List;
+import com.psc06.pojo.UserStoryData;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+
+public class ClientServerTest {
+
+    private ClientServer clientServer;
+
+    @Mock
+    private Client client;
+
+    @Mock
+    private WebTarget webTarget;
+
+    @Mock
+    private Response response;
+
+    @Before
+    public void setUp() {
+        client = ClientBuilder.newClient();
+        webTarget = client.target(String.format("http://%s:%s/rest/resource", "0.0.0.0", "8080"));
+    }
+
+    @Test
+    public void testRegisterUserStory() {
+        assertTrue(true);
+	}
+    /*
+    @Test
+	public void testRegisterSprint() {
+		
+	}
+    
+    @Test
+	public void testAssignUserStory() {
+		
+	}
+
+    @Test
+	public void testDeleteSprint() {
+		
+	}
+
+    @Test
+	public void testDeleteUserStory() {
+		
+	}
+
+    @Test
+	public void testReassignUserStory() {
+		
+	}
+
+    @Test
+	public void testGetAllUserStories() {
+
+	}
+
+    @Test
+	public List<UserStoryData> getUserStoriesFromSprint() {
+
+	}
+    */
+}
