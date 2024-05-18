84cb0d9cb80ccaf4e84d64a26d9d02f1322b35f4
nahiacfr <nahia.castaneda@opendeusto.es>
2024-05-18 19:40:37 +0200

    test SprintData

--- src/test/java/com/psc06/pojo/SprintDataTest.java
+++ src/test/java/com/psc06/pojo/SprintDataTest.java
@@ -13,16 +13,27 @@ public class SprintDataTest {
     public void setUp() {
         sprintData = new SprintData();
         sprintData.setSprintNum(23);
+        sprintData.setStartDate("2024-05-20");
+        sprintData.setEndDate("2024-06-10");
     }
 
     @Test
-    public void testGetId() {
+    public void testGetSprintNum() {
         assertEquals(23, sprintData.getSprintNum());
     }
 
     @Test
-    public void testToString() {
-        assertEquals("Sprint 23. ", sprintData.toString());
+    public void testGetStartDate() {
+        assertEquals("2024-05-20", sprintData.getStartDate());
     }
 
+    @Test
+    public void testGetEndDate() {
+        assertEquals("2024-06-10", sprintData.getEndDate());
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("Sprint 23. Start date: 2024-05-20, End date: 2024-06-10", sprintData.toString());
+    }
 }
